f022b6e7b3b3bb787141ca70bb900603cb851c68
M.Necati Tetik <119683909+MNecati@users.noreply.github.com>
2024-08-19 23:03:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)  # Portu 8081 olarak değiştirdim
+    app.run(host='0.0.0.0', port=8081) 
