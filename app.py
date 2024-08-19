53138e68f56b176d7eb5e65211bbcc593f30cc8e
M.Necati Tetik <119683909+MNecati@users.noreply.github.com>
2024-08-19 22:56:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
-
-app = Flask(_name_)
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+FROM python:3.9-slim
+WORKDIR /app
+COPY app.py /app
+RUN pip install Flask
+EXPOSE 8081  # Portu 8081 olarak değiştirdim
+CMD ["python", "app.py"]
